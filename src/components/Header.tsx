fd529f40c8202d35a316dcb655b25df72310fae0
Hj <jjjhjjjjj123@gmail.com>
2023-05-26 02:01:41 +0900

    Header 생성

---
+++ src/components/Header.tsx
@@ -0,0 +1,20 @@
+import Link from "next/link";
+
+const pages = ["home", "about", "posts", "contact"];
+
+export default function Header() {
+  return (
+    <header className="flex justify-between items-center">
+      <h1 className="text-2xl font-semibold">
+        <Link href="/">My blog</Link>
+      </h1>
+      <nav className="text-xl">
+        {pages.map((page) => (
+          <Link href={`/${page}`} className="px-2">
+            {page}
+          </Link>
+        ))}
+      </nav>
+    </header>
+  );
+}
